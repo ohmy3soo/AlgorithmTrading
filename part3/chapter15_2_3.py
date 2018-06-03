82002ed25ea2273fc39f1dd9ed868a73c812d638
ahn <ohmy3soo@nate.com>
2018-06-03 12:28:34 +0900

    bar 차트 그리기

--- part3/chapter15_2_3.py
+++ part3/chapter15_2_3.py
@@ -13,12 +13,24 @@ fluctuations = [1.83, 1.30, 1.30, 1.26, 1.06, 0.93, 0.77, 0.68, 0.65, 0.61]
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111)
 
+'''
+# 수평
 ypos = np.arange(10)
 rects = plt.barh(ypos, fluctuations, align='center', height=0.5)
 plt.yticks(ypos, industry)
 
 for i, rect in enumerate(rects):
     ax.text(0.95 * rect.get_width(), rect.get_y() + rect.get_height() / 2.0, str(fluctuations[i]) + '%', ha='right', va='center')
+'''
+
+
+# 수직
+pos = np.arange(10)
+rects = plt.bar(pos, fluctuations, align='center', width=0.5)
+plt.xticks(pos, industry)
+
+for i, rect in enumerate(rects):
+    ax.text(rect.get_x() + rect.get_width() / 2.0, 0.95 * rect.get_height(), str(fluctuations[i]) + '%', ha='center')
 
 plt.xlabel('등락률')
 plt.show()
